3933d9e982c1479a4541b1ed2553e0cfea1048c4
Andrej Amelichkin <ainshtein2004@gmail.com>
2017-02-24 00:32:45 +0200

    start makaron-project

---
+++ src/js/partials/slider.js
@@ -0,0 +1,16 @@
+// -------- SLIDER ------- //
+;$(document).ready(function() {
+
+  $(".owl-carousel").owlCarousel({
+
+      navigation : true,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem : true,
+      autoPlay : true,
+
+      items:1,
+      margin:0,
+      autoHeight:true
+  });
+});
